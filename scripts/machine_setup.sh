cd66c0924e7e400b0bc7afd65bfc06f265b90935
Sonny Laskar <sonnylaskar@gmail.com>
2020-04-26 10:35:01 +0000

    added -y to apt to avoid prompt

--- scripts/machine_setup.sh
+++ scripts/machine_setup.sh
@@ -9,7 +9,7 @@ git lfs install
 ### Install Python3.6 ###
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.6 python3.6-dev
+sudo apt-get install -y  python3.6 python3.6-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3.6 get-pip.py
 sudo ln -s /usr/bin/python3.6 /usr/local/bin/python3
